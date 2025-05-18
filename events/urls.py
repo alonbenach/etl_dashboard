3813120d7216417be8207d3d3c8bfbba576ef14b
Alon Benach <benach@gmail.com>
2025-05-18 22:17:25 +0200

    fix: move files into backend

--- events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import EventListCreateView, DailyMetricListView
-
-urlpatterns = [
-    path("events/", EventListCreateView.as_view(), name="event-list-create"),
-    path("metrics/", DailyMetricListView.as_view(), name="daily-metric-list"),
-]
